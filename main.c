f07f0b7611d5152414e2a8de11e919522c268a64
tracyandesia <uitracers@gmail.com>
2023-08-22 04:15:38 -0700

    added infinite loop and exit message

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main(int ac, char **argv)
 	char *prompt = "(SIMPLEShell) $ ";
 	char *lineptr;
 	size_t f = 0;
+	ssize_t fchars_read;
 
 	/*
 	 * declaring void variables to avoid flags 
@@ -17,10 +18,26 @@ int main(int ac, char **argv)
 	 */
 	(void)ac; (void)argv;
 
-	printf("%s", prompt);
-	getline(&lineptr, &f, stdin);
-	printf("%s\f", lineptr);
+	/*
+	 * now this part should 
+	 * handle and infinite
+	 * while loop
+	 */
+	while (1){
+		printf("%s", prompt);
+			fchars_read = getline(&lineptr, &f, stdin);
+		/*check if the getline function breaks or has reached an End of Line (EOF) or if a user presses CTRL + D*/
+		if (fchars_read == -1)
+		{
+			printf("Quiting shell...\n");
+			return (-1);
+		}
 
-	free(lineptr);
+		printf("%s\f", lineptr);
+
+		/*free up allocated memory*/
+		free(lineptr);
+	}
+
 	return (0);
 }
