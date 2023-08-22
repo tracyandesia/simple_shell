d1534686a4d62632b51f69d43c2ac494d5134b32
tracyandesia <uitracers@gmail.com>
2023-08-22 06:02:19 -0700

    passing commands typed into the shell

--- main.c
+++ main.c
@@ -5,39 +5,81 @@
  * comment here
  * return 0 - success
  */
-int main(int ac, char **argv)
+int main(void)
 {
-	char *prompt = "(SIMPLEShell) $ ";
-	char *lineptr;
+	char *full_command = NULL,
+	     *copy_command = NULL;
+	char **argv;
 	size_t f = 0;
-	ssize_t fchars_read;
+	/*number of char user types*/
+	ssize_t fchars_read; 
+	const char *delim = " \n";
+	int num_tokens = 0;
+	char *token;
+	int j;
 
-	/*
-	 * declaring void variables to avoid flags 
-	 * while compiling
-	 */
-	(void)ac; (void)argv;
+	/*strcpy(command_copy , full_command)*/
+	/*print a prompt for the user to type something*/
+	printf("$ ");
 
-	/*
-	 * now this part should 
-	 * handle and infinite
-	 * while loop
-	 */
-	while (1){
-		printf("%s", prompt);
-			fchars_read = getline(&lineptr, &f, stdin);
-		/*check if the getline function breaks or has reached an End of Line (EOF) or if a user presses CTRL + D*/
-		if (fchars_read == -1)
+	/*get string typed in the &  pass it to full_command*/
+	fchars_read = getline(&full_command, &f, stdin);
+
+	/*allocating space to store he char read by getline*/
+	copy_command= malloc(sizeof(char) * fchars_read);
+
+	if (copy_command == NULL)
+	{
+		perror("tsh: memory allocation has some errors");
+
+		return (-1);
+	}
+
+	/*make a copy of the typed  command*/
+	strcpy(copy_command, full_command);
+
+	/*checking the getline function - failed or reached EOF*/
+	if (fchars_read == -1)
+	{
+		printf("Quitting shell, deuces ✌️...\n");
+		return (-1);
+	}
+	else {
+		/*splitting string (full_command) into an array of words*/
+		token = strtok(full_command, delim);
+
+		/*allocating space - stores variable arg before we determ the tokens*/
+		while (token != NULL)
 		{
-			printf("Quiting shell...\n");
-			return (-1);
+			num_tokens++;
+			token = strtok(NULL, delim);
 		}
+		num_tokens++;
+		/*prinf(">>> %d \n", num_tokens);*/
 
-		printf("%s\f", lineptr);
+		/*malloc - allocating enough space for the pointer */
+		argv = malloc(sizeof(char *) * num_tokens);
 
-		/*free up allocated memory*/
-		free(lineptr);
+		token = strtok(copy_command, delim);
+
+		for (j = 0; token != NULL; j++)
+		{
+			argv[j] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[j], token);
+
+			/*printf(">> %s \n", argv[j]);*/
+			token = strtok(NULL, delim);
+		}
+		argv[j] = NULL;
+
+		/*exec the commands with execv*/
+
+		free(argv);
+		free(full_command);
+		free(copy_command);
 	}
 
 	return (0);
 }
+
+
